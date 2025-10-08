69c25fa482873b3f06e7c8ded6a0994b874b3f38
Kseniya Krasnokutska <kseniya.krasnokutska@adaware.com>
2025-10-08 00:54:44 -0400

    added color styles to reflect changing of Traffic Lights, modified postcss config , installed postcss dependancies

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+ plugins: {
+    '@tailwindcss/postcss': {}, // Tailwind plugin
+    'postcss-nesting': {},      // Enables CSS nesting (& support)
+    'autoprefixer': {},         // Adds vendor prefixes for compatibility
+  },
 };
 
 export default config;
